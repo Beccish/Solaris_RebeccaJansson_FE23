27820f3db3e6343655d2fe710645ad1db0fb5ccc
Rebecca <126094643+Beccish@users.noreply.github.com>
2024-02-22 15:14:13 +0100

    styling och förberedelse för js
    mestadels styling på sökboxen och förberedelse för js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('searchBox').addEventListener('input', function() {
+    var searchQuery = this.value; // Hämtar användarens sökterm
+    // Här kan du använda searchQuery med ditt JavaScript API
+});
